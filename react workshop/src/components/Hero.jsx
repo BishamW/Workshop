a6dad3fd9f5278840dc608d687ab85d401e6a4c1
Bisham Wagle <bishamw37@gmail.com>
2026-04-02 13:04:01 +0545

    hero section

---
+++ react workshop/src/components/Hero.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export const Hero = () => {
+  return (
+   <section className='flex items-center justify-center h-[600px] bg-gray-100 px-20'>
+    <div>
+<h1 className='text-5xl font-bold mb-4'> Welcome to the ISMT Chat, </h1>
+<p className='text-lg text-gray-600 mb-8'>Get started with our chat application and connect with others 
+     </p>
+     <button className='bg-blue-600 text-white px-6 py-2 rounded-md hover:bg-blue-400 cursor-pointer'>Get Started
+
+     </button>
+   
+   </div></section>
+  )
+}
--- react workshop/src/components/Navbar.jsx
+++ react workshop/src/components/Navbar.jsx
@@ -2,18 +2,18 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <header>
+    <header className='flex items-center justify-between px-20 py-8 bg-blue-50'>
         <div>
-            <h1>ISMT Chat</h1>
+            <h1 className='text-4xl font-bold text-blue-600'> ISMT Chat</h1>
         </div>
 
-        <div>
-            <div>
-                <a href="/Home">Home</a>
+        <div className='flex items-center gap-8'>
+            <div className='flex items-center gap-8 text-xl front-medium text-gray-600'>
+                <a href="/">Home</a>
                 <a href="/about">About</a>
                 <a href="/contact">Contact</a>
             </div>
-            <button>Chat page</button>
+            <button className='bg-blue-600 text-white px-6 py-2 rounded-md hover:bg-blue-400 cursor-pointer'>Chat page</button>
         </div>
     </header>
   )
